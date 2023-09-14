2bf06bb2a0c288d5231e877ac4fd9eeb0196d070
Bipon Roy <biponroy400@gmail.com>
2023-09-14 21:54:13 +0600

    Added Cart Component with Static Style

--- src/Component/Cards/Cards.jsx
+++ src/Component/Cards/Cards.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDollarSign, faBookOpen } from "@fortawesome/free-solid-svg-icons";
+import Cart from "../Cart/Cart";
 const Cards = () => {
     const [cards, setCards] = useState([]);
 
@@ -11,31 +12,31 @@ const Cards = () => {
             .then((data) => setCards(data));
     }, []);
     return (
-        <div className="max-w-[1340px] mx-auto">
-            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 mx-6 lg:mx-0">
+        <div className="max-w-[1340px] mx-auto my-2 flex flex-col lg:flex-row gap-5">
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 mx-6 lg:mx-0">
                 {cards.map((card) => (
                     <div key={card.id} className="card bg-base-100">
                         <figure className="bg-[#1111110D] mx-5 mt-6 mb-4 rounded-xl">
                             <img src={card.image} className="w-full" alt="Course Image" />
                         </figure>
                         <div className="space-y-2">
-                            <p className="text-[#1C1B1B] font-bold text-xl mx-6">
+                            <p className="text-[#1C1B1B] font-bold text-lg ml-6">
                                 {card.course_name}
                             </p>
-                            <p className="text-[#1111118d] text-lg mx-6">{card.course_details}</p>
+                            <p className="text-[#1111118d] text-base ml-6">{card.course_details}</p>
                         </div>
-                        <div className="flex mx-10 my-3 justify-between">
-                            <span className="text-[#1C1B1B] text-2xl ">
+                        <div className="flex mx-6 my-3 justify-between \">
+                            <span className="text-[#1C1B1B] text-xl ">
                                 <FontAwesomeIcon icon={faDollarSign} />
                             </span>
-                            <p className="text-[#1111118d] text-lg font-semibold">
+                            <p className="text-[#1111118d] text-base font-semibold">
                                 Price : {card.price}
                             </p>
 
-                            <span className="text-[#1C1B1B] text-2xl ">
+                            <span className="text-[#1C1B1B] text-xl ">
                                 <FontAwesomeIcon icon={faBookOpen} />
                             </span>
-                            <p className="text-[#1111118d] text-lg font-semibold">
+                            <p className="text-[#1111118d] text-base font-semibold">
                                 Credit : {card.credit}hr
                             </p>
                         </div>
@@ -44,6 +45,9 @@ const Cards = () => {
                         </button>
                     </div>
                 ))}
+            </div>
+            <div>
+                <Cart></Cart>
             </div>
         </div>
     );
---
+++ src/Component/Cart/Cart.jsx
@@ -0,0 +1,26 @@
+const Cart = () => {
+    return (
+        <div className="mx-6 lg:mx-0 lg:w-[300px]">
+            <div className=" py-6 bg-white rounded-xl px-6">
+                <h1 className="font-bold text-lg text-[#2F80ED] ">Credit Hour Remaining 7 hr</h1>
+                <div className="divider mx-auto my-2"></div>
+                <div className="space-y-2">
+                    <h1 className="font-bold text-lg text-[#1C1B1B] mb-4">Course Name</h1>
+                    <ol className="list-decimal text-base font-semibold space-y-2 ml-4">
+                        <li>Introduction to c programming</li>
+                        <li>Introduction to C++ for DSA</li>
+                        <li>Software Engineering</li>
+                    </ol>
+                </div>
+                <div>
+                    <div className="divider mx-auto my-1"></div>
+                    <h3 className="text-[#111111bd] text-lg font-semibold ">Total Credit Hour:</h3>
+                    <div className="divider  mx-auto my-2"></div>
+                    <h3 className="text-[#111111bd] text-lg font-semibold ">Total Price :</h3>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Cart;
